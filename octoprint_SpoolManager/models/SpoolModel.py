23b0a5ccef8d62c6e0ab2bb653e910e8bf2f6d4e
jkotowicz1 <59420781+jkotowicz1@users.noreply.github.com>
2024-01-04 12:07:56 +0100

    changed dataBaseId in BaseModel from Autofield to Integer, as peewee was throwing error that its not bolean

--- octoprint_SpoolManager/models/SpoolModel.py
+++ octoprint_SpoolManager/models/SpoolModel.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-from peewee import CharField, Model, DecimalField, FloatField, DateField, DateTimeField, TextField, IntegerField, \
-	BooleanField
+from peewee import CharField, Model, DecimalField, FloatField, DateField, DateTimeField, TextField, IntegerField,BooleanField
 
 from octoprint_SpoolManager.models.BaseModel import BaseModel
 
