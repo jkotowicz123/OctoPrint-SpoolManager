30c541c4a022b5159e6c92462a003dabd3b5dcfb
jkotowicz1 <59420781+jkotowicz1@users.noreply.github.com>
2024-01-04 17:27:39 +0100

    added square image left to qrcode

--- octoprint_SpoolManager/api/SpoolManagerAPI.py
+++ octoprint_SpoolManager/api/SpoolManagerAPI.py
@@ -584,13 +584,13 @@ class SpoolManagerAPI(octoprint.plugin.BlueprintPlugin):
 
 			from PIL import Image, ImageDraw, ImageFont, ImageColor
 			#imageFileLocation = self._basefolder + "/static/images/SPMByOlli.png"
-			fontBig = ImageFont.truetype("arial.ttf", 18)
-			fontSmall = ImageFont.truetype("arial.ttf", 12)
+			fontBig = ImageFont.truetype("Lato-Regular.ttf", 18)
+			fontSmall = ImageFont.truetype("Lato-Regular.ttf", 12)
 
 			textImage = Image.new(mode = "RGB", size = (290,290), color = "white")
 			textDraw = ImageDraw.Draw(textImage)
-			textDraw.text((10, 20), displayName,fill="black",font=fontBig)
-			textDraw.text((10, 50), databaseId,fill="black",font=fontsmall)
+			textDraw.text((10, 20), "spoolModel.displayName",fill="black",font=fontBig)
+			textDraw.text((10, 50), databaseId,fill="black",font=fontSmall)
 
 		
 
