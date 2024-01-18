fcfb7e2b24be8d7a6ab64eae2325672323dec1d5
jkotowicz1 <59420781+jkotowicz1@users.noreply.github.com>
2024-01-18 12:22:58 +0100

    added vendor and material on label, gotta check to select final label design

--- octoprint_SpoolManager/api/SpoolManagerAPI.py
+++ octoprint_SpoolManager/api/SpoolManagerAPI.py
@@ -590,13 +590,17 @@ class SpoolManagerAPI(octoprint.plugin.BlueprintPlugin):
 			fontBig = ImageFont.truetype(self._basefolder + "/static/fonts/Lato-Bold.ttf", 48)
 			fontSmall = ImageFont.truetype(self._basefolder + "/static/fonts/Lato-Regular.ttf", 36)
 			displayNameBroken= textwrap.wrap(spoolModel.displayName,10)
+			material = spoolModel.material
+			vendor = spoolModel.vendor
 
 			textImage = Image.new(mode = "RGB", size = (290,290), color = "white")
 			textDraw = ImageDraw.Draw(textImage)
 			textDraw.text((10, 20),displayNameBroken[0],fill="black",font=fontBig)
 			displayNameBroken=displayNameBroken[1:]
 			displayNameLower = "".join(displayNameBroken)
-			textDraw.text((10,90), displayNameLower,fill="black",font=fontSmall)
+			textDraw.text((10,80), displayNameLower,fill="black",font=fontBig)
+			textDraw.text((10,120), material,fill="black",font=fontSmall)
+			textDraw.text((10,140), vendor,fill="black",font=fontSmall)
 			textDraw.text((10,160), databaseId,fill="black",font=fontExtraBig)
 			
 
