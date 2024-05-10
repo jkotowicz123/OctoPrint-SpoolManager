c776f919dacc6cb1315b51270f7def4bee5a2025
jkotowicz1 <59420781+jkotowicz1@users.noreply.github.com>
2024-05-10 15:29:37 +0200

    reverted projectfilter to != all

--- octoprint_SpoolManager/DatabaseManager.py
+++ octoprint_SpoolManager/DatabaseManager.py
@@ -997,7 +997,7 @@ class DatabaseManager(object):
 				materialFilter = tableQuery["materialFilter"]
 				vendorFilter = tableQuery["vendorFilter"]
 				colorFilter = tableQuery["colorFilter"]
-				#projectFilter = tableQuery["projectFilter"]
+				projectFilter = tableQuery["projectFilter"]
 				# materialFilter
 				# u'ABS,PLA'
 				# u''
@@ -1028,13 +1028,13 @@ class DatabaseManager(object):
 				# u'my filaments, Client XYZ'
 				# u''
 				# u'all'
-				#projectFilter = StringUtils.to_native_str(projectFilter)
-				#if (projectFilter == "all"):
-				#	if (StringUtils.isEmpty(projectFilter)):
-				#		myQuery = myQuery.where( (SpoolModel.project == '') )
-				#	else:
-				#		allProjects = projectFilter.split(",")
-				#		myQuery = myQuery.where(SpoolModel.project.in_(allProjects))
+				projectFilter = StringUtils.to_native_str(projectFilter)
+				if (projectFilter != "all"):
+					if (StringUtils.isEmpty(projectFilter)):
+						myQuery = myQuery.where( (SpoolModel.project == '') )
+					else:
+						allProjects = projectFilter.split(",")
+						myQuery = myQuery.where(SpoolModel.project.in_(allProjects))
 				#		# for vendor in allVendors:
 				#		# 	myQuery = myQuery.orwhere((SpoolModel.vendor == vendor))
 				# colorFilter
@@ -1101,6 +1101,11 @@ class DatabaseManager(object):
 					myQuery = myQuery.order_by(SpoolModel.databaseId.desc())
 				else:
 					myQuery = myQuery.order_by(SpoolModel.databaseId.asc())
+			if ("project" == sortColumn):
+				if ("desc" == sortOrder):
+					myQuery = myQuery.order_by(SpoolModel.project.desc())
+				else:
+					myQuery = myQuery.order_by(SpoolModel.project.asc())
 	
 			self._logger.info("Quering spools: %s" % myQuery)
 			return myQuery
