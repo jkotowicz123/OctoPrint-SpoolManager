ce8b4e79f931641ae4319b9361c9a488acbaa3e8
jkotowicz1 <59420781+jkotowicz1@users.noreply.github.com>
2024-06-13 16:34:04 +0200

    looks like it just returns spoolId, so no wonder it couldnt jsonify it, will try without json.dumps

--- octoprint_SpoolManager/api/SpoolManagerAPI.py
+++ octoprint_SpoolManager/api/SpoolManagerAPI.py
@@ -528,8 +528,8 @@ class SpoolManagerAPI(octoprint.plugin.BlueprintPlugin):
 
 		# TODO QR-Code pre-select always tool0 and then the edit-dialog is shown. Better approach: show dialog and the user could choose
 		spoolModel = self._selectSpool(0, databaseId,printerNumber)
-		json_object = json.dumps(spoolModel, indent = 4)
-		return flask.Response(json_object, status=200, mimetype='application/json')
+		#json_object = json.dumps(spoolModel, indent = 4)
+		return flask.Response(spoolModel, status=200, mimetype='application/json')
 	
 	
 		#spoolModelAsDict = None
