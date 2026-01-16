7e3f4c2d2f3c7dc63de58a7f1df5eda247fa7961
jkotowicz1 <59420781+jkotowicz1@users.noreply.github.com>
2026-01-16 18:10:50 +0100

    added endpoint for assign print with Zebra handheld scanner

--- octoprint_SpoolManager/common/SettingsKeys.py
+++ octoprint_SpoolManager/common/SettingsKeys.py
@@ -6,6 +6,7 @@ class SettingsKeys():
 	SETTINGS_KEY_SELECTED_SPOOLS_DATABASE_IDS = "selectedSpoolsDatabaseIds"
 	SETTINGS_KEY_HIDE_EMPTY_SPOOL_IN_SIDEBAR = "hideEmptySpoolsInSidebar"
 	SETTINGS_KEY_HIDE_INACTIVE_SPOOL_IN_SIDEBAR = "hideInactiveSpoolsInSidebar"
+	SETTINGS_KEY_SHOW_SHEET_MAGAZINE_IN_SIDEBAR = "showSheetMagazineInSidebar"
 
 	SETTINGS_KEY_REMINDER_SELECTING_SPOOL = "reminderSelectingSpool"
 	SETTINGS_KEY_WARN_IF_SPOOL_NOT_SELECTED = "warnIfSpoolNotSelected"
